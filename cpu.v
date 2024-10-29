025ee9ebd11b94ca27857b07cfe84e45b351cf5e
Jonathan Houser <150945630+JonathanHouser27@users.noreply.github.com>
2024-10-29 05:14:05 -0500

    troubleshooting

--- cpu.v
+++ cpu.v
@@ -10,7 +10,8 @@ wire [31:0] current_pc;
 wire [31:0] inst_encoding;
 wire [31:0] rf_out_0;
 wire [31:0] rf_out_1;
-wire [31:0] next_pc;
+wire [31:0] rf_data_in;
+    // wire [31:0] next_pc; 10/29
 wire [31:0] pc_plus_4;
 wire [31:0] alu_out;
 wire [31:0] dmem_out;
@@ -29,7 +30,7 @@ wire branch_enable;
 wire [31:0] imm;
 
 // Calculate PC + 4
-assign pc_plus_4 = current_pc + 4;
+//assign pc_plus_4 = current_pc + 4; 10/29
 
 // Program Counter instantiation
 Program_Counter pc_module(
@@ -57,9 +58,9 @@ decode decode(
     .instruction(inst_encoding),
     .clk(clk),
     .rst(rst),
-    .Rs1(Rs1),
-    .Rs2(Rs2),
-    .Rd(Rd),
+  //  .Rs1(Rs1), 10/29 have the rf separate these
+  //  .Rs2(Rs2),
+  //  .Rd(Rd),
     .alu_op(alu_op),
     .imm(imm),  // Single immediate output
     .alu_src(alu_src),
@@ -69,12 +70,14 @@ decode decode(
     .branch(branch_enable)
 );
 
+assign rf_data_in = dmem_read ? dmem_out : alu_out;
+    
 // Register File instantiation
 reg_file rf(
-    .Rs1(Rs1),
-    .Rs2(Rs2),
-    .Rd(Rd),
-    .data_in(dmem_read ? dmem_out : alu_out),  // Select between memory and ALU output
+    .Rs1(inst_encoding[19:15]),
+    .Rs2(inst_encoding[24:20]),
+    .Rd(inst_encoding[11:7]),
+    .data_in(rf_data_in),  // move logic to the top 10/29
     .we(reg_write),
     .read_data1(rf_out_0),
     .read_data2(rf_out_1),
