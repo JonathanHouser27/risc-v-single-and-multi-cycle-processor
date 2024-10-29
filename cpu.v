b6d46927799be124514e177bfc114095698ee25e
Jonathan Houser <150945630+JonathanHouser27@users.noreply.github.com>
2024-10-29 07:55:06 -0500

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -11,11 +11,12 @@ wire [31:0] inst_encoding;
 wire [31:0] rf_out_0;
 wire [31:0] rf_out_1;
 wire [31:0] rf_data_in;
-    // wire [31:0] next_pc; 10/29
 wire [31:0] pc_plus_4;
 wire [31:0] alu_out;
 wire [31:0] dmem_out;
+wire [31:0] branch_target;
 wire alu_c_out;
+wire branch_taken;
 
 // from decode
 wire reg_write;
@@ -29,83 +30,84 @@ wire [4:0] Rd;
 wire branch_enable;
 wire [31:0] imm;
 
-// Calculate PC + 4
-//assign pc_plus_4 = current_pc + 4; 10/29
 
 // Program Counter instantiation
 Program_Counter pc_module(
-    .clk(clk),
-    .rst(rst),
-    .branch_addy(alu_out),  // Use ALU output for branch address
-    .branch_enable(branch_enable),
-    .PC_out(current_pc)
+    .clk(clk),				// Input
+    .rst(rst),				// Input
+    .branch_addy(branch_target),	// Input
+    .branch_enable(branch_taken),	// Input
+    .PC_out(current_pc)			// Output
 );
 
 // Instruction Memory instantiation
 memory2c imem(
-    .data_out(inst_encoding),
-    .data_in(32'b0),
-    .addr(current_pc),
-    .enable(1'b1),
-    .wr(1'b0),
-    .createdump(1'b0),
-    .clk(clk),
-    .rst(rst)
+    .data_out(inst_encoding),		// Output
+    .data_in(32'b0),			// Input
+    .addr(current_pc),			// Input
+    .enable(1'b1),			// Input
+    .wr(1'b0),				// Input
+    .createdump(1'b0),			// Input
+    .clk(clk),				// Input
+    .rst(rst)				// Input
 );
 
 // decoder instantiation
 decode decode(
-    .instruction(inst_encoding),
-    .clk(clk),
-    .rst(rst),
-  //  .Rs1(Rs1), 10/29 have the rf separate these
-  //  .Rs2(Rs2),
-  //  .Rd(Rd),
-    .alu_op(alu_op),
-    .imm(imm),  // Single immediate output
-    .alu_src(alu_src),
-    .write_enable(reg_write),
-    .mem_read(dmem_read),
-    .mem_write(dmem_write),
-    .branch(branch_enable)
+    .instruction(inst_encoding),	// Input
+    .Rs1_out(Rs1),			// Output
+    .Rs2_out(Rs2),			// Output
+    .Rd_out(Rd),			// Output
+    .clk(clk),				// Input
+    .rst(rst),				// Input
+    .alu_op(alu_op),			// Output
+    .imm(imm),  			// Output
+    .alu_src(alu_src),			// Output
+    .we(reg_write),			// Output
+    .mem_read(dmem_read),		// Output
+    .mem_write(dmem_write),		// Output
+    .branch_target(branch_target),	// Output
+    .branch_enable(branch_enable)	// Output
 );
 
-assign rf_data_in = dmem_read ? dmem_out : alu_out;
-    
+
 // Register File instantiation
 reg_file rf(
-    .Rs1(inst_encoding[19:15]),
-    .Rs2(inst_encoding[24:20]),
-    .Rd(inst_encoding[11:7]),
-    .data_in(rf_data_in),  // move logic to the top 10/29
-    .we(reg_write),
-    .read_data1(rf_out_0),
-    .read_data2(rf_out_1),
-    .clk(clk),
-    .rst(rst)
+    .Rs1(Rs1),				// Input
+    .Rs2(Rs2),				// Input
+    .Rd(Rd),				// Input
+    .data_in(rf_data_in),		// Input
+    .we(reg_write),			// Input
+    .read_data1(rf_out_0),		// Output
+    .read_data2(rf_out_1),		// Output
+    .clk(clk),				// Input
+    .rst(rst)				// Input
 );
 
 // ALU instantiation
 ALU alu(
-    .A(rf_out_0),
-    .B(rf_out_1),
-    .imm(imm),
-    .alu_src(alu_src),
-    .func(alu_op),
-    .out(alu_out),
-    .c_out(alu_c_out)
+    .A(rf_out_0),			// Input
+    .B(rf_out_1),			// Input
+    .imm(imm),				// Input
+    .alu_src(alu_src),			// Input
+    .func(alu_op),			// Input
+    .out(alu_out),			// Output
+    .c_out(alu_c_out),			// Output
+    .branch_taken(branch_taken)		// Output
 );
 
 // DMEM instantiation
 memory2c dmem(
-    .data_out(dmem_out),
-    .data_in(rf_out_1),  // Store data from register
-    .addr(alu_out),      // Use ALU output as address
-    .enable(dmem_read | dmem_write),  // Enable on read or write
-    .wr(dmem_write),
-    .createdump(1'b0),
-    .clk(clk),
-    .rst(rst)
+    .data_out(dmem_out),		// Output
+    .data_in(alu_out),  		// Input
+    .addr(alu_out), 			// Input   
+    .enable(dmem_read),  		// Input
+    .wr(dmem_write),			// Input
+    .createdump(1'b0),			// Input
+    .clk(clk),				// Input
+    .rst(rst)				// Input
 );
+
+assign rf_data_in = dmem_read ? dmem_out : alu_out;
 
 endmodule
