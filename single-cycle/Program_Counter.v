999d0171c5f04e017616bd48878aaa25de71f072
Jonathan Houser <150945630+JonathanHouser27@users.noreply.github.com>
2025-02-25 11:08:21 -0600

    Update Program_Counter.v

--- single-cycle/Program_Counter.v
+++ single-cycle/Program_Counter.v
@@ -1,25 +1,31 @@
 `timescale 1us/100ns
 
 module Program_Counter(
-    input clk,
-    input rst,                // Reset signal
-    input [31:0] branch_addy, // Address to jump to on a branch
-    input branch_enable,      // Branch enable signal
-    output [31:0] PC_out      // PC output as a wire
+    input clk,                          // Clock signal
+    input rst,                          // Reset signal
+    input [31:0] branch_addy,           // Address to jump to on a branch
+    input branch_enable,                // Branch enable signal
+    output [31:0] PC_out                
 );
 
-wire [31:0] next_PC;          // Next value of the program counter
+wire [31:0] next_PC;                    // Next value of the program counter
+wire [31:0] current_pc;
 
-assign next_PC = (rst) ? 32'b0 :		// Reset
-		 (branch_enable) ? branch_addy:	// Branch
-		 (PC_out + 32'd4);		// plus 4
+// Determine next PC based on jump or branch
+assign next_PC = (rst) ? 32'b0 :       			// Reset to 0 (or another value)
+	         (branch_enable) ? current_pc + branch_addy :	// Branch
+                 (current_pc + 32'd4);           		// Normal PC increment
 
+// Program Counter Register
 register pc_register (
     .clk(clk),
-    .rst(rst),
-    .we(1'b1),
-    .d_input(next_PC),
-    .out(PC_out)
+    .rst(rst),                           // Reset signal passed to register
+    .we(1'b1),                           // Always write-enable
+    .d_input(next_PC),                   // Input to PC register (next_PC)
+    .out(current_pc)                      // Output of PC register
 );
+
+// Output PC as the full 32-bit value
+assign PC_out = next_PC;
 
 endmodule
